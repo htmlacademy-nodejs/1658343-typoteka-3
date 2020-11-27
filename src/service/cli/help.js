dbf189bd48570f32e405f403eb978d20fa988d63
leshareva <leshareva.box@gmail.com>
2020-11-27 10:18:30 +0700

    added chalk and async

--- src/service/cli/help.js
+++ src/service/cli/help.js
@@ -1,16 +1,18 @@
 "use strict";
 
+const chalk = require(`chalk`);
+
 module.exports = {
   name: `--help`,
   run: () => {
-    console.log(`Программа запускает http-сервер и формирует файл с данными для API.
+    console.log(chalk.grey(`Программа запускает http-сервер и формирует файл с данными для API.
     
     Гайд:
     service.js <command>
     Команды:
     --version:            выводит номер версии
     --help:               печатает этот текст
-    --generate <count>    формирует файл mocks.json`);
+    --generate <count>    формирует файл mocks.json`));
   },
 };
 
